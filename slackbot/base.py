1ae1aa38da509f0cfd7dcf105aefaec385832adf
Maria Andriesa <maria.andriesa@paddypowerbetfair.com>
2022-12-19 15:18:38 +0200

    adding var for the ID of the currently authenticated user

--- slackbot/base.py
+++ slackbot/base.py
@@ -13,7 +13,7 @@ class MessageProcessor:
     def __init__(self, rtm_client, web_client) -> None:
         self.client = rtm_client
         self.web = web_client
-        self.auth_user_id = self.web.auth_test()["user_id"]
+        self.user_id = self.web.auth_test()["user_id"]
 
     @staticmethod
     def user_has_perm(user, perm):
